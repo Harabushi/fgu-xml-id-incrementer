a240be04bf1f35c01e6f73f5490a6e75711dc9df
Colby Harris <harris.colby@gmail.com>
2022-05-15 19:48:11 -0500

    added CLI functionality

--- change-tags.js
+++ change-tags.js
@@ -1,40 +1,37 @@
 let fs = require('fs');
 
+const args = process.argv.slice(2);
+
+let currentNum = args[2] ? args[2] - 1 : 0;
 let output = '';
-let currentNum = 0;
 let newXml = '';
 
 function num() {
   currentNum++;
   output = ('0000' + currentNum).slice(-5)
-  // console.log(output);
   return;
 }
 
 function readWriteSync() {
-  // this should be the name of the input file
-  let data = fs.readFileSync('Threeitemsexport.xml', 'utf-8');
+  // this should be the name of the input file, default to input.xml
+  let data = fs.readFileSync(args[0] ? `${args[0]}` : 'input.xml', 'utf-8');
 
   data.split(/\r?\n/).forEach(line => {
-    // console.log(`Line from file: ${line}`)
     if (line.match('<id>')) {
       num();
       let newValue = line.replace('<id>', `<id-${output}>`)
       newXml += `\n${newValue}`;
-      console.log(newXml);
     } else if (line.match('</id>')) {
       currentNum--;
       num();
       let newValue = line.replace('</id>', `</id-${output}>`)
       newXml += `\n${newValue}`;
-      console.log(newXml);
     } else {
       newXml += `\n${line}`;
     }
   })
-  // this should be whatever you want the name of the output file to be
-  fs.writeFileSync('./output/output.xml', newXml, 'utf-8');
-  // console.log('readFileSync complete');
+  // this should be whatever you want the name of the output file to be, default to output.xml
+  fs.writeFileSync(args[1] ? `./output/${args[1]}` : './output/output.xml', newXml, 'utf-8');
 }
 
 readWriteSync();
